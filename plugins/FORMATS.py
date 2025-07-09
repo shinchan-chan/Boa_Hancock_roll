f32325d8a1ae29d7e863da8368976666d6e7bdba
shinchan-chan <shinchanadamopyt21@gmail.com>
2025-07-09 14:43:37 +0530

    Update FORMATS.py

--- plugins/FORMATS.py
+++ plugins/FORMATS.py
@@ -39,7 +39,7 @@ HELP_TEXT = """<b>⁉️ Hᴇʏ...!! {mention} ~
 <b><i>◈ Sᴛɪʟʟ ʜᴀᴠᴇ ᴅᴏᴜʙᴛs, ᴄᴏɴᴛᴀᴄᴛ ʙᴇʟᴏᴡ ᴘᴇʀsᴏɴs/ɢʀᴏᴜᴘ ᴀs ᴘᴇʀ ʏᴏᴜʀ ɴᴇᴇᴅ !</i></b>"""
 
 ABOUT_TXT = """<b>🤖 ᴍʏ ɴᴀᴍᴇ: {botname}
-<blockquote><b>» ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/Save_ish>𝐒ᴀᴠᴇ 𝐒αᴍᴀ</a>\n» ᴋ-ᴅʀᴀᴍᴀ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/K_Drama_In_Hindi_English>𝐊-𝐃ʀᴀᴍᴀ 𝐇ᴜsʜ</a>\n» ᴏᴡɴᴇʀ : <a href=https://t.me/Save_ish>𝐒ᴀᴠᴇ 𝐒ᴀᴍᴀ ֟፝</a></blockquote></b>"""
+<blockquote><b>» ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/Save_ish>𝐒ᴀᴠᴇ 𝐒αᴍᴀ</a>\n» ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/+jaGChj0sIl42NGY9>𝐇ᴇʟʟғɪʀᴇ ᴍᴏᴠɪᴇ</a>\n» ᴏᴡɴᴇʀ : <a href=https://t.me/Save_ish>𝐒ᴀᴠᴇ 𝐒ᴀᴍᴀ ֟፝</a></blockquote></b>"""
 
 SETTING_TXT = """<b>»  Cᴏɴғɪɢᴜʀᴀᴛɪᴏɴs</b>
 <blockquote expandable>» ᴛᴏᴛᴀʟ ғᴏʀᴄᴇ sᴜʙ ᴄʜᴀɴɴᴇʟ:  <b>{total_fsub}</b>
